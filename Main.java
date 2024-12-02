04aaa435064facd910bd5a2091e27cf2c2410b65
farah <farah>
2024-12-02 20:23:57 +0100

    ajout de la classe main avec methode d'addition

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Résultat: " + addition(5, 3));
+    }
+}
